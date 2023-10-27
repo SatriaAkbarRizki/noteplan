1058f2506694f9ab42f85e83d7b858b815cf7929
SatriaAkbarRizki <sartiapl@gmail.com>
2023-10-28 00:40:39 +0700

    remake widget for improve perfomance on view login

--- lib/color/colors.dart
+++ lib/color/colors.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 class MyColors {
-  static Color colorBackgroundLoginOne = Color(0xffADC4CE);
-  static Color colorBackgroundLogonTwo = Color(0xffD9D9D9);
-  static Color colorBackgroundHome = Color(0xffF8F0E5);
+  static Color colorBackgroundLoginOne = const Color(0xffADC4CE);
+  static Color colorBackgroundLogonTwo = const Color(0xffD9D9D9);
+  static Color colorBackgroundHome = const Color(0xffF8F0E5);
   static Color colorCancel = Colors.red;
-  static Color colorButtonLogin = Color(0xffC7EBB3);
+  static Color colorButtonLogin = const Color(0xffC7EBB3);
 }
